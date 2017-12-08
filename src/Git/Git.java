c31c2469d6f506f5c03c86a9a74dc1f92eff5522
shemyka <akku.jenn@gmail.com>
2017-12-08 14:59:21 +0800

    bf2

--- src/Git/Git.java
+++ src/Git/Git.java
@@ -4,6 +4,6 @@ public class Git {
     public static void main(String[] agrs){
         System.out.print("Git Test");
         System.out.print("Git Test 2");
-        System.out.print("Bug fix");
+        System.out.print("Bug fix 2");
     }
 }
