f9b7803c50b101754c64c7f7e9166d4f87c2b892
shemyka <akku.jenn@gmail.com>
2017-12-08 14:38:38 +0800

    4

--- src/Git/Git.java
+++ src/Git/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] agrs){
         System.out.print("Git Test");
         System.out.print("Git Test 2");
+        System.out.print("Git Test 3");
     }
 }
